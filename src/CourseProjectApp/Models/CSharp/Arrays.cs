a8ead7fa5d1008ec95d93ec5d287a8361fc58e47
Drake Scott <dascott0204@gmail.com>
2017-04-14 00:50:16 -0500

    Implementing Arrays in C#!!!

---
+++ src/CourseProjectApp/Models/CSharp/Arrays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseProjectApp.Models.CSharp
+{
+    public class Arrays
+    {
+        // Declare Array
+
+        string[] firstArray;
+
+        // Declare and Initialize Array
+        string[] secondArray = new string[2];
+
+        // Declare and Initialize Array Inline
+        private string[] thirdArray = {"FirstElement", "SecondElement","ThirdElement"};
+
+        public void ArrayMethod()
+        {
+            secondArray[0] = "first";
+            secondArray[1] = "second";
+
+            foreach (var myElement in thirdArray)
+            {
+                Console.WriteLine(myElement);
+            }
+        }
+
+    }
+}
