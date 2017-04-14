15b0d8f16e450edeea674651c26032ed238e59a4
Drake Scott <dascott0204@gmail.com>
2017-04-14 01:04:25 -0500

    Interfaces in C# and how to implement them with your classes.

---
+++ src/CourseProjectApp/Models/CSharp/MyInterface.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseProjectApp.Models.Interface;
+
+namespace CourseProjectApp.Models.CSharp
+{
+    public class MyInterface: IInterface
+    {
+
+        private double amount;
+
+        private string value;
+
+        public MyInterface()
+        {
+            amount = 10.0;
+            value = "My String Value!!";
+        }
+
+        public void ShowValue()
+        {
+            Console.WriteLine($"My Value is:{value}");
+        }
+
+        public double GetValue()
+        {
+            return amount;
+        }
+    }
+
+    public class MyInterface2:IInterface
+    {
+        public void ShowValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetValue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
